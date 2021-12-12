83770f043472bb7f650a6e2a1aa5dec16bc1df08
Frank Wang <yf.frank.wang@outlook.com>
2021-12-12 14:51:51 +0800

    change time window to 1 hour

--- src/main/java/com/amazonaws/samples/kaja/taxi/consumer/ProcessTaxiStream.java
+++ src/main/java/com/amazonaws/samples/kaja/taxi/consumer/ProcessTaxiStream.java
@@ -123,8 +123,8 @@ public class ProcessTaxiStream {
 						return td.taxiType + ":" + td.geohash;
 					}
 				})
-				//collect all events in 5 minutes window
-				.window(TumblingEventTimeWindows.of(Time.minutes(5)))
+				//collect all events in 1 hour time window
+				.window(TumblingEventTimeWindows.of(Time.hours(1)))
 				//count events per geo hash in the time window
 				.apply(new CalcByGeoHash());
 
